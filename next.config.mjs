34a2298c80520d9155c5b1325f2b40ebee529a13
nsbweb <narendrabait@gmail.com>
2026-01-23 02:41:20 +0530

    Production Test image issues

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,11 @@
 const nextConfig = {
   reactCompiler: true,
   output: 'export',
+  basePath: "", // keep empty if served at domain root
+  assetPrefix: "", // same as above
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
